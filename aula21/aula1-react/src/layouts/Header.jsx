2c53665470be59dba430efbc6d074a56c168684b
julia <mariajuliaamaraldasilva@gmail.com>
2025-02-01 11:56:51 -0300

    REACT / COMPONENTS / EXPORTAÇÃO E IMPORTAÇÃO / CHILDRENS

---
+++ aula21/aula1-react/src/layouts/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <>
+
+        <header className=" vh-10 bg-dark text-white">
+            <div className="d-flex justify-content-between p-3">
+            <h1>logo</h1>
+           
+        
+        <div>
+            menu
+        </div>
+        </div>
+        </header>
+        
+        </>
+    )
+}
+
+export default Header;
---
+++ aula21/aula1-react/src/layouts/Main.jsx
@@ -0,0 +1,9 @@
+function Main(){
+    return(
+        <>
+        <h1>Main - Componente</h1>
+        </>
+    )
+}
+
+export default Main;
